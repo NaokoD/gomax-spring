b913e4649dd7d2c719e2f9e99badee68112db61b
ydia <yannickdia31@gmail.com>
2020-02-14 11:31:54 +0100

    Add update class Film and create migration V5

--- src/main/java/com/gomax/entities/Film.java
+++ src/main/java/com/gomax/entities/Film.java
@@ -1,5 +1,6 @@
 package com.gomax.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,11 +18,17 @@ public class Film {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String titre;
-    private Long duree;
-
-
-    @ManyToOne
-    private Categorie categorie;
+    @JsonProperty("Title")
+    private String title;
+    @JsonProperty("Runtime")
+    private String runtime;
+    @JsonProperty("Genre")
+    private String genre;
+    @JsonProperty("Plot")
+    private String plot;
+    @JsonProperty("Actors")
+    private String actors;
+    @JsonProperty("Poster")
+    private String poster;
 
 }
